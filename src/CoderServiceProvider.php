84e8cc8d3cfabf1f6a0fed8922424aaae6b229d3
Charles Bilbo <charlesbilbo@CHARLIE-BILBO.local>
2021-10-20 15:51:22 -0500

    finish package to overload HandleCors

--- src/CoderServiceProvider.php
+++ src/CoderServiceProvider.php
@@ -2,12 +2,30 @@
 
 namespace RXMG\Coder;
 
+use Fruitcake\Cors\HandleCors;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use Mockery;
 
 class CoderServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if(App::environment('local') && env('CODER', false)) {
 
+            $mock = Mockery::mock(HandleCors::class)
+                ->makePartial()
+                ->shouldAllowMockingProtectedMethods()
+                ->allows(
+                    [
+                        'shouldRun' => false
+                    ]
+                );
+
+            app()->instance(
+                HandleCors::class,
+                $mock
+            );
+        }
     }
 }
